6f2ee77747d3c134be1f75583a374d2991683af3
spiderOO7 <shingwl@yahoo.com>
2016-06-25 17:17:29 +0530

    basic project done. custom Toast learnt.

--- app/src/main/java/com/example/spider/derek1/MainActivity.java
+++ app/src/main/java/com/example/spider/derek1/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.spider.derek1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText personName;
+
+    public void getReferences() {
+        personName = (EditText) findViewById(R.id.personName);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getReferences();
     }
+
+    public void yesCommand(View view) {
+        String name = String.valueOf(personName.getText());
+        String message = name + " you choose Yes!";
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+
+//      Set a custom Toast.
+//      View v = getLayoutInflater().inflate(R.layout.toast,null,false);
+//      toast.setView(v);
+        toast.show();
+    }
+
+    public void noCommand(View view) {
+        String name = String.valueOf(personName.getText());
+        String message = name + " you choose No!";
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
 }
